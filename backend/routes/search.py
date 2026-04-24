dcf0f1c7c9e3181410eac92de5f7a467226cd70d
Ciro Torres <ciro_3@hotmail.com>
2026-04-24 14:40:47 -0300

    primeiro nova branch

--- backend/routes/search.py
+++ backend/routes/search.py
@@ -4,7 +4,7 @@ from services.external_api import get_item
 router = APIRouter()
 
 @router.get("/")
-def search_movies():
-    response = get_item()
+def search_movies(q: str = "Frankenstein"):
+    response = get_item(q)
     return response
 
--- backend/services/external_api.py
+++ backend/services/external_api.py
@@ -1,10 +1,10 @@
 import requests
 from core.config import API_KEY, API_BASE_URL
 
-def get_item():
+def get_item(query: str):
     params = {
-        "q": "dragon-ball",
-        "type": "show",
+        "q": query,
+        "type": "movie",
         "limit": 3,
         "info": 1,
         "k": API_KEY
@@ -13,4 +13,13 @@ def get_item():
     response = requests.get(API_BASE_URL, params=params)
     data = response.json()
 
-    return data
+    return [    
+        {
+            "name": item.get("name"),
+            "yID": item.get("yID"),
+            "description": item.get("description"),
+            "wUrl": item.get("wUrl"),
+            "yUrl": item.get("yUrl")
+        }
+        for item in data.get("similar", {}).get("results", [])
+    ]
