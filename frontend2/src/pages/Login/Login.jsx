d9306e468375f69faeda1a461d808d3fa61a5b04
Ciro Torres <ciro_3@hotmail.com>
2026-05-08 13:36:30 -0300

    improving login page (card)

--- frontend2/src/pages/Login/Login.jsx
+++ frontend2/src/pages/Login/Login.jsx
@@ -6,6 +6,7 @@ import appleIcon from '../../assets/apple.svg'
 function Login() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [flip, setFlip] = useState(true);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -14,6 +15,10 @@ function Login() {
 
   return (
     <div className='screen'>
+      <button className="switch" onClick={() => setFlip(true)}>on</button>
+      <button className="switch" onClick={() => setFlip(false)}>off</button>
+
+    {flip? 
       <section className='card'>
         <header className='header'>
           <h1 className='title'>Entrar</h1>
@@ -23,10 +28,10 @@ function Login() {
           <button className='iconsocial'>
           <img src={googleIcon} alt="Google" className='imagemsocial' />
           Google
-        </button>
+          </button>
 
         <button className='iconsocial'>
-          <img src={appleIcon} alt="Apple" className='imagemsocial' />
+          <img src={appleIcon} alt="Apple" className='imagemsocial' style={{height: 32}} />
           Apple
         </button>
         </div>
@@ -70,10 +75,74 @@ function Login() {
             Entrar
           </button>
         </form>
-         <p className="signup-text">Ainda não tem conta? <a href="#" className="">Crie aqui!</a></p>
+         <p className="signup-text">Ainda não tem conta? <a href="/signup" 
+         className="">Crie aqui!</a></p>
 
+         
       </section>
-    </div>
+      :
+      <section className='card'>
+        <header className='header'>
+          <h1 className='title'>Criar conta</h1>
+          <p className='subtitle'>Crie com sua conta</p>
+        </header>
+        <div className='social'>
+          <button className='iconsocial'>
+          <img src={googleIcon} alt="Google" className='imagemsocial' />
+          Google
+          </button>
+
+        <button className='iconsocial'>
+          <img src={appleIcon} alt="Apple" className='imagemsocial' style={{height: 32}} />
+          Apple
+        </button>
+        </div>
+
+        <p className='separador'>ou</p>
+
+        <form className='form' onSubmit={handleSubmit}>
+          <div className='field'>
+            <label className='label' htmlFor="email">
+              Email
+            </label>
+            <input
+              id="email"
+              className='input'
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              placeholder="teste@exemplo.com"
+              autoComplete="email"
+              required
+            />
+          </div>
+
+          <div className='field'>
+            <label className='label' htmlFor="password">
+              Senha
+            </label>
+            <input
+              id="password"
+              className='input'
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              placeholder="••••••••"
+              autoComplete="current-password"
+              required
+            />
+          </div>
+
+          <button type="submit" className='button'>
+            Entrar
+          </button>
+        </form>
+         <p className="signup-text">Ainda não tem conta? <a href="/signup" 
+         className="">Crie aqui!</a></p>
+
+         
+      </section> }
+    </div> 
   );
 }
 
